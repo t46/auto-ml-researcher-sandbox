b7d85fccac966c78b20d67fdb7ab1dcc9103149c
t46 <takagi4646@gmail.com>
2024-03-19 15:21:32 +0900

    change method proposition to module

--- method_proposition/proposed_method_generation.py
+++ method_proposition/proposed_method_generation.py
@@ -1,55 +1,63 @@
 from openai import OpenAI
 
-prompt = """
-Generate the best idea to solve the problem described below.
-
-Problem:
-{problem}
-"""
-
-# TODO: OpenAI の返答を生成する部分を関数・メソッド化する
-
-client = OpenAI()
-response = client.chat.completions.create(
-    model="gpt-4-0125-preview",
-    temperature=0.0,
-    messages=[{"role": "user", "content": prompt},
-              {"role": "system", "content": "You are a machine learning researcher."}],
-    )
-
-with open('proposed_method.txt', 'w') as f:
-    f.write(response.choices[0].message.content)
+def generate_proposed_method(problem: str, client: OpenAI) -> str:
 
 
-prompt = f"""
-Formulate the following proposed method mathematically.
+    prompt = f"""
+    Generate the best idea to solve the problem described below.
 
-Proposed Method:
-{response.choices[0].message.content}
-"""
+    Problem:
+    {problem}
+    """
 
-response = client.chat.completions.create(
-    model="gpt-4-0125-preview",
-    temperature=0.0,
-    messages=[{"role": "user", "content": prompt},
-              {"role": "system", "content": "You are a machine learning researcher."}],
-    )
+    # TODO: OpenAI の返答を生成する部分を関数・メソッド化する
+    response = client.chat.completions.create(
+        model="gpt-4-0125-preview",
+        temperature=0.0,
+        messages=[{"role": "user", "content": prompt},
+                {"role": "system", "content": "You are a machine learning researcher."}],
+        )
 
-prompt = f"""
-Further concretize and formalize the following proposed method and regenerate the updated proposed method.
+    with open('proposed_method.txt', 'w') as f:
+        f.write(response.choices[0].message.content)
 
-Proposed Method:
-{response.choices[0].message.content}
-"""
+    prompt = f"""
+    Formulate the following proposed method mathematically.
 
-response = client.chat.completions.create(
-    model="gpt-4-0125-preview",
-    temperature=0.0,
-    messages=[{"role": "user", "content": prompt},
-              {"role": "system", "content": "You are a machine learning researcher."}],
-    )
+    Proposed Method:
+    {response.choices[0].message.content}
+    """
 
-with open('formulated_proposed_method.txt', 'w') as f:
-    f.write(response.choices[0].message.content)
+    response = client.chat.completions.create(
+        model="gpt-4-0125-preview",
+        temperature=0.0,
+        messages=[{"role": "user", "content": prompt},
+                {"role": "system", "content": "You are a machine learning researcher."}],
+        )
 
-print(response.choices[0].message.content)
+    prompt = f"""
+    Further concretize and formalize the following proposed method and regenerate the updated proposed method.
+
+    Proposed Method:
+    {response.choices[0].message.content}
+    """
+
+    response = client.chat.completions.create(
+        model="gpt-4-0125-preview",
+        temperature=0.0,
+        messages=[{"role": "user", "content": prompt},
+                {"role": "system", "content": "You are a machine learning researcher."}],
+        )
+
+    with open('formulated_proposed_method.txt', 'w') as f:
+        f.write(response.choices[0].message.content)
+
+    return response.choices[0].message.content
+
+
+if __name__ == "__main__":
+    client = OpenAI()
+    with open("./research_problem.txt") as f:
+        research_problem = f.read()
+    proposed_method = generate_proposed_method(research_problem, client)
+    print(proposed_method)
